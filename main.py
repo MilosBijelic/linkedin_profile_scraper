60718a6694e38c896a28801c97c73464d11e34be
milos.bijelic <mbijelic.03@gmail.com>
2023-01-09 20:47:09 -0500

    Changed to OOP

--- main.py
+++ main.py
@@ -1,92 +1,115 @@
 import re
+import time
+import csv
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import time
-import csv
 
-# Creating a webdriver instance
-driver = webdriver.Chrome("C:/Users/milos.bijelic/Downloads/chromedriver_win32/chromedriver.exe")
-# This instance will be used to log into LinkedIn
+class LinkedinScraper():
+    def __init__(self):
+        # Creating a webdriver instance
+        self.driver = webdriver.Chrome("C:/Users/milos.bijelic/Downloads/chromedriver_win32/chromedriver.exe")
+        # This instance will be used to log into LinkedIn
 
-# Opening linkedIn's login page
-driver.get("https://linkedin.com/uas/login")
+    # create
+    def _create_csv(self):
+        # Creates .csv file to write to
+        headers = ['Name', 'Job Title', 'Company']
+        with open('spire-member-list.csv', 'a', encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow(headers)
 
-# waiting for the page to load
-time.sleep(2)
+    def create_member_list(self):
+        # If the .csv exists delete the old copy
+        if os.path.exists("spire-member-list.csv"):
+            os.remove("spire-member-list.csv")
+            self._create_csv()
+        else:
+            self._create_csv()
 
-# Find the Username box on the webpage
-username = driver.find_element(By.ID, "username")
+    def start_chrome(self):
+        # Opening linkedIn's login page
+        self.driver.get("https://linkedin.com/uas/login")
 
-# ENTER YOUR EMAIL ADDRESS HERE
-username.send_keys("mbijelic.03@gmail.com")
+        # waiting for the page to load
+        time.sleep(2)
 
-# Find the Password box on the webpage
-pword = driver.find_element(By.ID, "password")
-# In case of an error, try changing the element
-# tag used here.
+        # Find the Username box on the webpage
+        username = self.driver.find_element(By.ID, "username")
 
-# ENTER YOUR PASSWORD HERE
-pword.send_keys("MilosBij1996")
+        # ENTER YOUR EMAIL ADDRESS HERE
+        username.send_keys("mbijelic.03@gmail.com")
 
-# Clicking on the login button
-driver.find_element(By.CLASS_NAME, "login__form_action_container").click()
+        # Find the Password box on the webpage
+        pword = self.driver.find_element(By.ID, "password")
+        # In case of an error, try changing the element
+        # tag used here.
 
-# Wait 15 seconds in case of security test
-time.sleep(15)
+        # ENTER YOUR PASSWORD HERE
+        pword.send_keys("MilosBij1996")
 
-# ENTER ALL THE LINKEDIN URLS TO HIT, SEPARATED BY COMMAS
-spire_linkedin_urls = ["https://www.linkedin.com/in/filip-o-8bb46a52", "https://www.linkedin.com/in/milosbijelic"]
+        # Clicking on the login button
+        self.driver.find_element(By.CLASS_NAME, "login__form_action_container").click()
 
-# Creates .csv file to write to
-headers = ['Name', 'Job Title', 'Company']
-with open('spire-member-list.csv', 'a', encoding='UTF8', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(headers)
+        # Wait 15 seconds in case of security test
+        time.sleep(15)
 
-# Loops through all entries in the spire_linkedin_urls list
-for url in spire_linkedin_urls:
-    # Opens up the URL
-    driver.get(url)
+    def scrape_linkedin(self, url_list):
+        # Loops through all entries in the spire_linkedin_urls list
+        for url in url_list:
+            # Opens up the URL
+            self.driver.get(url)
 
-    # Stores the source code of the webpage
-    src = driver.page_source
-    soup = BeautifulSoup(src, 'lxml')
+            # Stores the source code of the webpage
+            src = self.driver.page_source
+            soup = BeautifulSoup(src, 'lxml')
 
-    # Get their headline
-    intro = soup.find('div', {'class': 'pv-text-details__left-panel'})
+            # Get their headline
+            intro = soup.find('div', {'class': 'pv-text-details__left-panel'})
 
-    name_loc = intro.find("h1")
+            name_loc = intro.find("h1")
 
-    # Extract the Name
-    name = name_loc.get_text().strip()
+            # Extract the Name
+            name = name_loc.get_text().strip()
 
-    works_at_loc = intro.find("div", {'class': 'text-body-medium'})
+            works_at_loc = intro.find("div", {'class': 'text-body-medium'})
 
-    # Extracting the persons header
-    works_at = works_at_loc.get_text().strip()
+            # Extracting the persons header
+            works_at = works_at_loc.get_text().strip()
 
-    # Empty to store data
-    data_row = []
+            # Empty to store data
+            data_row = []
 
-    # Extract the title and company from their headline
-    title = re.search("^(.+?),(.*)", works_at)
-    company = re.search("(.*) at (.*)", works_at)
+            # Extract the title and company from their headline
+            title = re.search("^(.+?),(.*)", works_at)
+            company = re.search("(.*) at (.*)", works_at)
 
-    # push the name, title and company to the list
-    data_row.append(name)
+            # push the name, title and company to the list
+            data_row.append(name)
 
-    if title is None:
-        data_row.append(title)
-    else:
-        data_row.append(title.group(1))
+            if title is None:
+                data_row.append(title)
+            else:
+                data_row.append(title.group(1))
 
-    if company is None:
-        data_row.append(company)
-    else:
-        data_row.append(company.group(2))
+            if company is None:
+                data_row.append(company)
+            else:
+                data_row.append(company.group(2))
 
-    # write the users info to the .csv
-    with open('spire-member-list.csv', 'a', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(data_row)
+            # write the users info to the .csv
+            with open('spire-member-list.csv', 'a', encoding='UTF8', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(data_row)
+
+
+
+if __name__ == "__main__":
+    url_list = ['https://www.linkedin.com/in/milosbijelic', 'https://www.linkedin.com/in/filip-o-8bb46a52']
+
+    scraper = LinkedinScraper()
+
+    scraper.create_member_list()
+    scraper.start_chrome()
+    scraper.scrape_linkedin(url_list)
